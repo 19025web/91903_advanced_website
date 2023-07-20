137c9c9840f04ffe733d8d8fd6791f835a577f1f
19025web <110867485+19025web@users.noreply.github.com>
2023-07-21 10:13:03 +1200

    Add files via upload

---
+++ js/total.js
@@ -0,0 +1,29 @@
+function add() 
+    {
+      var num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, result; 
+        num1 = parseInt(document.getElementById("input1").value) * 3;
+        num2 = parseInt(document.getElementById("input2").value) * 5;
+        num3 = parseInt(document.getElementById("input3").value) * 5;
+        num4 = parseInt(document.getElementById("input4").value) * 4;
+        num5 = parseInt(document.getElementById("input5").value) * 3;
+        num6 = parseInt(document.getElementById("input6").value) * 6;
+        num7 = parseInt(document.getElementById("input7").value) * 5;
+        num8 = parseInt(document.getElementById("input8").value) * 10;
+        num9 = parseInt(document.getElementById("input9").value) * 10;
+        num10 = parseInt(document.getElementById("input10").value) * 3;
+      
+        result = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+
+        document.getElementById("answer").value = result;
+  }
+
+    function submitForm() {
+   // Get the first form with the name
+   // Usually the form name is not repeated
+   // but duplicate names are possible in HTML
+   // Therefore to work around the issue, enforce the correct index
+   var frm = document.getElementsByName('info')[0];
+   frm.submit(); // Submit the form
+   frm.reset();  // Reset all form data
+   return false; // Prevent page refresh
+    }
