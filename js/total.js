888de3f7e2b6b22d6f0e9f0deefd2477ca9a0391
19025web <110867485+19025web@users.noreply.github.com>
2023-07-31 13:46:48 +1200

    Add files via upload

--- js/total.js
+++ js/total.js
@@ -1,15 +1,15 @@
 function add() {
   var num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, result;
-  num1 = parseInt(document.getElementById("input1").value) * 3;
-  num2 = parseInt(document.getElementById("input2").value) * 5;
-  num3 = parseInt(document.getElementById("input3").value) * 5;
-  num4 = parseInt(document.getElementById("input4").value) * 4;
-  num5 = parseInt(document.getElementById("input5").value) * 3;
-  num6 = parseInt(document.getElementById("input6").value) * 6;
-  num7 = parseInt(document.getElementById("input7").value) * 5;
-  num8 = parseInt(document.getElementById("input8").value) * 10;
-  num9 = parseInt(document.getElementById("input9").value) * 10;
-  num10 = parseInt(document.getElementById("input10").value) * 3;
+  num1 = parseInt(document.getElementById("big-cherry-keychain").value) * 3;
+  num2 = parseInt(document.getElementById("blue-triceratops").value) * 5;
+  num3 = parseInt(document.getElementById("boats").value) * 5;
+  num4 = parseInt(document.getElementById("milk-tea").value) * 4;
+  num5 = parseInt(document.getElementById("chicken").value) * 3;
+  num6 = parseInt(document.getElementById("dango").value) * 6;
+  num7 = parseInt(document.getElementById("pink-triceratops").value) * 5;
+  num8 = parseInt(document.getElementById("pastel-stars").value) * 10;
+  num9 = parseInt(document.getElementById("stars").value) * 10;
+  num10 = parseInt(document.getElementById("crane-in-a-jar").value) * 3;
 
   result = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
 
